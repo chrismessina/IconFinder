ca01e0daea7e39b27506352220f21d0debb57eda
Chris Messina <chris.messina@gmail.com>
2025-08-16 15:16:58 -0700

    Add View menu toggle for Hide/Show Duplicates and exact-duplicate filtering via SHA-256 with cached hashes

--- IconFinder/JRAppDelegate.m
+++ IconFinder/JRAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "JRAppDelegate.h"
+#import <CommonCrypto/CommonCrypto.h>
 
 
 @interface JRAppDelegate ()
@@ -17,6 +18,10 @@
 @property (strong) NSPredicate *filterPredicate;
 @property (readonly) NSMutableArray *filteredImagePaths;
 @property (strong) NSTask *findTask;
+
+// Dedupe support
+@property (assign) BOOL hideDuplicates;
+@property (strong) NSMutableDictionary<NSString*, NSString*> *pathToHash; // path -> sha256 hex
 
 @end
 
@@ -58,6 +63,15 @@ static NSArray *imageTypes;
 	NSFileManager *fm = [NSFileManager defaultManager];
 	self.filters = [NSMutableSet set];
 	self.filterPredicate = [NSPredicate predicateWithValue:YES];
+	self.hideDuplicates = [[NSUserDefaults standardUserDefaults] boolForKey:@"HideDuplicates"];
+	
+	// Load hash cache if present
+	if ([fm fileExistsAtPath:[self hashCacheFilePath]]) {
+		NSDictionary *cache = [NSDictionary dictionaryWithContentsOfFile:[self hashCacheFilePath]];
+		self.pathToHash = [cache mutableCopy];
+	} else {
+		self.pathToHash = [NSMutableDictionary dictionary];
+	}
 	
 	if ([fm fileExistsAtPath:[self imagesPlistFilePath]])
 	{
@@ -165,7 +179,7 @@ static NSArray *imageTypes;
 {
 	NSFileManager *fm = [NSFileManager defaultManager];
 	BOOL isDirectory = NO;
-	NSString *appSupport = [NSHomeDirectory() stringByAppendingFormat:@"/Library/Application Support/IconFinder"];
+	NSString *appSupport = [NSHomeDirectory() stringByAppendingPathComponent:@"/Library/Application Support/IconFinder"];
 	
 	if ( !([fm fileExistsAtPath:appSupport isDirectory:&isDirectory] && isDirectory) )
 	{
@@ -173,18 +187,44 @@ static NSArray *imageTypes;
 	}
 	
 	NSString *imagePathsFile = [appSupport stringByAppendingPathComponent:@"imagePaths.plist"];
-	
 	[self.imagePaths writeToFile:imagePathsFile atomically:YES];
+	// Persist hash cache as well
+	if (self.pathToHash) {
+		[self.pathToHash writeToFile:[self hashCacheFilePath] atomically:YES];
+	}
 }
 
 - (NSString*)imagesPlistFilePath
 {
-	return [NSHomeDirectory() stringByAppendingFormat:@"/Library/Application Support/IconFinder/imagePaths.plist"];
+	return [NSHomeDirectory() stringByAppendingPathComponent:@"/Library/Application Support/IconFinder/imagePaths.plist"];
+}
+
+- (NSString*)hashCacheFilePath
+{
+	return [NSHomeDirectory() stringByAppendingPathComponent:@"/Library/Application Support/IconFinder/hashCache.plist"];
 }
 
 - (NSMutableArray*)filteredImagePaths
 {
-	return [[self.imagePaths filteredArrayUsingPredicate:self.filterPredicate] mutableCopy];
+	NSArray *base = [self.imagePaths filteredArrayUsingPredicate:self.filterPredicate];
+	if (!self.hideDuplicates) {
+		return [base mutableCopy];
+	}
+	// Hide exact duplicates by SHA-256; keep first occurrence
+	NSMutableArray *result = [NSMutableArray arrayWithCapacity:[base count]];
+	NSMutableSet *seen = [NSMutableSet set];
+	for (NSString *path in base) {
+		NSString *hash = [self sha256ForFileAtPath:path];
+		if (hash == nil) {
+			[result addObject:path];
+			continue;
+		}
+		if (![seen containsObject:hash]) {
+			[seen addObject:hash];
+			[result addObject:path];
+		}
+	}
+	return result;
 }
 
 - (NSPredicate*)filterPredicate
@@ -234,6 +274,51 @@ static NSArray *imageTypes;
 		[self findImages];
 		self.imagePaths = [NSMutableArray array];
 	}
+}
+
+- (IBAction)toggleHideDuplicates:(id)sender
+{
+	self.hideDuplicates = !self.hideDuplicates;
+	[[NSUserDefaults standardUserDefaults] setBool:self.hideDuplicates forKey:@"HideDuplicates"];
+	if ([sender isKindOfClass:[NSMenuItem class]]) {
+		NSMenuItem *item = (NSMenuItem *)sender;
+		[item setTitle:(self.hideDuplicates ? @"Show Duplicates" : @"Hide Duplicates")];
+		[item setState:(self.hideDuplicates ? NSControlStateValueOn : NSControlStateValueOff)];
+	}
+	[self willChangeValueForKey:@"imagePaths"];
+	[self didChangeValueForKey:@"imagePaths"];
+}
+
+#pragma mark - Hashing
+
+- (NSString *)sha256ForFileAtPath:(NSString *)path
+{
+	if (!path) return nil;
+	NSString *cached = [self.pathToHash objectForKey:path];
+	if (cached) return cached;
+	NSInputStream *stream = [NSInputStream inputStreamWithFileAtPath:path];
+	if (!stream) return nil;
+	[stream open];
+	CC_SHA256_CTX ctx;
+	CC_SHA256_Init(&ctx);
+	uint8_t buffer[64 * 1024];
+	NSInteger read = 0;
+	while ((read = [stream read:buffer maxLength:sizeof(buffer)]) > 0) {
+		CC_SHA256_Update(&ctx, buffer, (CC_LONG)read);
+	}
+	[stream close];
+	if (read < 0) return nil;
+	unsigned char digest[CC_SHA256_DIGEST_LENGTH];
+	CC_SHA256_Final(digest, &ctx);
+	NSMutableString *hex = [NSMutableString stringWithCapacity:CC_SHA256_DIGEST_LENGTH * 2];
+	for (int i = 0; i < CC_SHA256_DIGEST_LENGTH; i++) {
+		[hex appendFormat:@"%02x", digest[i]];
+	}
+	NSString *result = [hex copy];
+	if (result) {
+		[self.pathToHash setObject:result forKey:path];
+	}
+	return result;
 }
 
 @end
